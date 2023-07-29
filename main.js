17ece4d930a7a72387991b1c82ca564f18bb03d6
Bernice Dwight <BDWIGHTNJ21@GMAIL.COM>
2023-07-29 19:14:02 -0400

    created sections

--- main.js
+++ main.js
@@ -7,8 +7,20 @@ function submitForm() {
 
     var input1=document.getElementById("input1").value;
     var input2=document.getElementById("input2").value;
+    
+    <button onclick="submitForm()">Submit</button>
+    
     var input3=document.getElementById("input3").value;
 
     console.log("Input 1:" + input1);
+    console.log("Input 2:" + input2);
+    console.log("Input 3:" + input3);
+
+
+
+
+
+
+    
     
 }
