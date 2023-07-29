80a4ca21cdcc040e81d29ac71b3355129daba4a1
Bernice Dwight <BDWIGHTNJ21@GMAIL.COM>
2023-07-29 05:33:10 -0400

    created empty array to store data

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 console.log ('connected')
+// create an empty array to store input data
+var data =[];
+
+//Declare input data
+function submitForm() {
+
+    var input1=document.getElementById("input1").value;
+    var input2=document.getElementById("input2").value;
+    var input3=document.getElementById("input3").value;
+
+    console.log("Input 1:" + input1);
+    
+}
