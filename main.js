75c9c07f3d99a1b1e3d360e99644cb83fe8ab446
Bernice Dwight <BDWIGHTNJ21@GMAIL.COM>
2023-07-30 21:27:40 -0400

    correct changes

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function submitForm() {
     var input1=document.getElementById("input1").value;
     var input2=document.getElementById("input2").value;
     
-    <button onclick="submitForm()">Submit</button>
+    
     
     var input3=document.getElementById("input3").value;
 
