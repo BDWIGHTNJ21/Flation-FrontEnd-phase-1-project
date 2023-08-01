632b600e72f59c1823c1edb80820bb1a1e84571f
Bernice Dwight <bdwightnj21@gmail.com>
2023-08-01 10:24:27 -0400

    add eventlistener with using focus

--- main.js
+++ main.js
@@ -1,26 +1,24 @@
-console.log ('connected')
+console.log("main.js connected");
+// activate evenlistener using forcus and forcus on events within a form
+
+const handleFormInputFocus = () => {
+  console.log("focus occurred");
+};
+const assignedUserInput = document.body.querySelector("#assigned-user");
+
+assignedUserInput.addEventListener("focus", handleFormInputFocus);
+
 // create an empty array to store input data
-var data =[];
+var data = [];
 
 //Declare input data
 function submitForm() {
+  var input1 = document.getElementById("input1").value;
+  var input2 = document.getElementById("input2").value;
 
-    var input1=document.getElementById("input1").value;
-    var input2=document.getElementById("input2").value;
-    
-    
-    
-    var input3=document.getElementById("input3").value;
+  var input3 = document.getElementById("input3").value;
 
-    console.log("Input 1:" + input1);
-    console.log("Input 2:" + input2);
-    console.log("Input 3:" + input3);
-
-
-
-
-
-
-    
-    
+  console.log("Input 1:" + input1);
+  console.log("Input 2:" + input2);
+  console.log("Input 3:" + input3);
 }
