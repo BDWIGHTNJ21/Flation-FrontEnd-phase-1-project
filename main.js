63b36672ba780dccc0474dba6a3e207a01052e84
Bernice Dwight <BDWIGHTNJ21@GMAIL.COM>
2023-08-05 02:39:13 -0400

    render

--- main.js
+++ main.js
@@ -3,24 +3,30 @@ console.log("main.js connected");
 // activate evenlistener using forcus and forcus on events within a form
 const searchTermsInput = document.body.querySelector("#search-terms");
 
+const renderMealCategories = () => {
+  console.log('renderMealCategories')
+}
+
 const getMealCategories = async() =>{
   const mealCategoriesApiURL = "https://www.themealdb.com/api/json/v1/1/categories.php"
 
   try{
     const response = await fetch(mealCategoriesApiURL)
-    const data =response.json()
-   console.log(`data:`, data); 
+    const data = await response.json()
+    const categories = await data.categories 
+   console.log(`categories:`, categories); 
   } catch (error){
   console.log(error)
   alert('Something went wrong,try again later')
 }
 
-  
+  return categories
 }
 const handleFormInputFocus = async () => {
   console.log("focus occurred"); 
   
-  await getMealCategories ();
+ const mealCategoriesObj= await getMealCategories ();
+ renderMealCategories();
 };
 
 searchTermsInput.addEventListener("focus", handleFormInputFocus);
