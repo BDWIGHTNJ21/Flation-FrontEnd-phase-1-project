d60437bb7c5446cfac881c4a9dc531052e9b0fee
Bernice Dwight <BDWIGHTNJ21@GMAIL.COM>
2023-08-06 02:52:24 -0400

    update repo

--- main.js
+++ main.js
@@ -3,30 +3,45 @@ console.log("main.js connected");
 // activate evenlistener using forcus and forcus on events within a form
 const searchTermsInput = document.body.querySelector("#search-terms");
 
-const renderMealCategories = () => {
-  console.log('renderMealCategories')
-}
-
-const getMealCategories = async() =>{
+// fetch meal categories data from TheMeal BD api
+const getMealCategories = async() => {
   const mealCategoriesApiURL = "https://www.themealdb.com/api/json/v1/1/categories.php"
 
   try{
     const response = await fetch(mealCategoriesApiURL)
-    const data = await response.json()
+    const data = a1wait response.json()
     const categories = await data.categories 
    console.log(`categories:`, categories); 
+   return categories;
   } catch (error){
   console.log(error)
   alert('Something went wrong,try again later')
 }
 
-  return categories
+
+
+
 }
+// render meal catefories data to dom
+const renderMealCategories = (mealCategoriesObj) => {
+  console.log('renderMealCategories')
+  console.table(mealCategoriesObj)
+            
+  
+              // Things that completed
+  // find and select a ul with meal categories ID dom elemt to appened data
+  const mealCategoriesList=document.body.querySelector('#meal-categories-list')
+  mealCategoriesArray.forEach(mealCategory =>{
+
+  });
+
+}
+//function to call when form input is given focus
 const handleFormInputFocus = async () => {
   console.log("focus occurred"); 
   
  const mealCategoriesObj= await getMealCategories ();
- renderMealCategories();
+ renderMealCategories(mealCategoriesObj);
 };
-
+// add eventlistener to search term input
 searchTermsInput.addEventListener("focus", handleFormInputFocus);
