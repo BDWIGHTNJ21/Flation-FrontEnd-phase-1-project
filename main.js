39d87e99f57e473c39a894fde80e17c904c6a33c
Bernice Dwight <BDWIGHTNJ21@GMAIL.COM>
2023-08-07 17:37:58 -0400

    created a new Ap

--- main.js
+++ main.js
@@ -1,79 +1,123 @@
 console.log("main.js connected");
 
 // activate evenlistener using forcus and forcus on events within a form
-const searchTermsInput = document.body.querySelector("#search-terms");
+//const searchTermsInput = document.body.querySelector("#search-terms");
+//--------------------New---------------------------------
+function carsData() {
+    const searchInput = document.getElementById('searchInput').value.toLowerCase();
+
+    function carsDataData() {
+        const searchInput = document.getElementById('searchInput').value.toLowerCase();
+      
+        fetch('data.json')
+          .then(response => response.json())
+          .then(data => {
+            const filteredData = data.filter(item => item.category.toLowerCase().includes(searchInput));
+            displayResults(filteredData);
+          })
+          .catch(error => console.error('Error:', error));
+      }
+      
+      function displayResults(data) {
+        const resultsDiv = document.getElementById('results');
+        resultsDiv.innerHTML = '';
+      
+        if (data.length === 0) {
+          resultsDiv.textContent = 'No results found.';
+          return;
+        }
+
+        const table = document.createElement('table');
+        const tableHeader = document.createElement('tr');
+        tableHeader.innerHTML = '<th>Brand</th><th>Model</th><th>Type</th>';
+        table.appendChild(tableHeader);
+      
+        data.forEach(item => {
+          const row = document.createElement('tr');
+          row.innerHTML = `<td>${item.brand}</td><td>${item.model}</td><td>${item.type}</td>`;
+          table.appendChild(row);
+        });
+      
+        resultsDiv.appendChild(table);
+      };
+// fetch Car type data from  BD api
 
 
-// fetch meal categories data from TheMeal BD api
-const getMealCategories = async() => {
-  const mealCategoriesApiURL = "https://www.themealdb.com/api/json/v1/1/categories.php"
-
-  try{
-    const response = await fetch(mealCategoriesApiURL);
-    const data = await response.json();
-    const categories = await data.categories; 
-   console.log(`categories:`, categories); 
-   return categories;
-  } catch (error){
-  console.log(error);
-  alert('Something went wrong,try again later');
+function addCars() {
+    const cars = document.getElementById('cars').value;
+    
+    const Brand = document.getElementById('Brand').value;
+    const model =  document.getElementById('model').value;
+    const type = document.getElementById('type').value;
+    
+    const carsData = {
+      brand : brand ,
+      model: model,
+      type: type,
+    };
+    
+    fetch('data.json')
+      .then(response => response.json())
+      .then(data => {
+        data.push(carsData);
+        return data;
+      })
+      .then(uptypedData => {
+        fetch('data.json', {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(uptypedData)
+        });
+      })
+      .catch(error => console.error('Error:', error));
+    
+    displayCars();
+  }
+  
+  function returnCars() {
+    const returnInput = document.getElementById('return').value;
+    
+    fetch('data.json')
+      .then(response => response.json())
+      .then(data => {
+        const uptypedData = data.filter(item => item.type !== returnInput);
+        return uptypedData;
+      })
+      .then(uptypedData => {
+        fetch('data.json', {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(uptypedData)
+        });
+      })
+      .catch(error => console.error('Error:', error));
+    
+    displayCars();
+  }
+  
+  function displayCars() {
+    fetch('data.json')
+      .then(response => response.json())
+      .then(data => {
+        const tableBody = document.getElementById('carsData');
+        tableBody.innerHTML = '';
+      
+        data.forEach(item => {
+          const row = document.createElement('tr');
+          row.innerHTML = `
+            <td>${item.brand}</td>
+            <td>${item.model}</td>
+            <td>${item.type}</td>
+            `;
+          tableBody.appendChild(row);
+        });
+      })
+      .catch(error => console.error('Error:', error))
+    
+    }
+  displayCars() 
 }
-};
-// render meal categories data to dom
-const renderMealCategories = (mealCategoriesArray) => {
-  console.log('renderMealCategories');
-  console.table(mealCategoriesArray);
-
-            
-  
-  // find and select a ul with meal categories ID dom element to appened data
-  const mealCategoriesList=document.body.querySelector('#meal-categories-list')
-  mealCategoriesArray.forEach(mealCategory => {
- 
- //create wrapping element <li>
- const mealCategoryListItem=document.createElement('li');
- mealCategoryListItem.className='meal-category-card';
- // apprehend child li.card to selected DOM element
- mealCategoriesList.appendChild(mealCategoryListItem);
-  //display the category name, image and description
- 
- //----------------------Meal Categories-------------
- //------IMG----------------IMAG CATEGORIES RENDERS FROM API TO DOM(WEBPAGE)
- // create an Meal Categories iMg elements START ADDING IMG
-const mealCategoryImg=document.createElement('img');
-//set img src to category thrumbnail url
-mealCategoryImg.src=mealCategory?.strCategoryThumb
-//give it a mobile friendly max width of like 300px
-mealCategoryImg.with=300
-//apprendChild the image to our card  LAST IMAGE CUSTOMLIZATION
-mealCategoryListItem.appendChild(mealCategoryImg);
-
-// ----NAME-------------------display catagories names-----------
-//create an he 4 element
-const mealCategoryName= document.createElement('h4');
-//set h4 element textContent to be meal category name
-mealCategoryListItem.appendChild(mealCategoryName);
-//apprendChild h4 to the card
-mealCategoryListItem.appendChild(mealCategoryName);
-
-//DESCRIPTION----------------- OF ELEMENT
-//create an paragraph elemment
-const mealCategoryDescription = document.createElement('p');
-// set p element textContent to be meal category description 
-mealCategoryDescription.textContent=mealCategory?.strCategoryDescription
-// appendChild the p to our card
-mealCategoryListItem.appendChild(mealCategoryDescription);
-  });
-
-};
-
-
-//function to call when form input is given focus
-const handleFormInputFocus = async () => {
-  console.log("focus occurred"); 
-  
- const mealCategoriesArray= await getMealCategories ();
- renderMealCategories(mealCategoriesArray);
-};
-// add eventlistener to search term input
-searchTermsInput.addEventListener("focus", handleFormInputFocus);
